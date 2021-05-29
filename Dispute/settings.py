457e1e516204427c8903cc4686835c702f2b326a
Alexander Krot <m8295360@gmail.com>
2021-05-29 20:07:52 +0300

    Maybe i can something 1.0

--- Dispute/settings.py
+++ Dispute/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_DIR = os.path.join(BASE_DIR,  'static')
-STATICFILES_DIRS = [STATIC_DIR,]
+#STATICFILES_DIRS = [STATIC_DIR,]
 STATIC_URL = '/static/'
 
 # Django Registration
